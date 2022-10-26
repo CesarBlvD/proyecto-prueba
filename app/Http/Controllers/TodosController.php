6c88f2d0b902c5c1e43fd54b5b589904f05c641c
Cesar Antonio Hernandez Valtierra <86888362+CesarBlvD@users.noreply.github.com>
2022-10-26 11:50:21 -0500

    Agregando csrf

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Todo;
 
 class TodosController extends Controller
 {
-    //
+    /**
+     * index para mostrar todos los todos
+     * store para guardar un todo
+     * update para actualizar un todo
+     * destroy para eliminar un todo
+     * edit para mostrar el formulario de edicion
+     **/
+    public function store(Request $request){
+        // Validamos
+        $request -> validate([
+            'title' => 'required|min:3'
+        ]);
+        // Creamos el objeto y asignamos los valores
+        $todo = new Todo;
+        $todo->title = $request->title;
+        $todo->save();
+        // redirigiendo al usuaro a la ruta todos con succes
+        return redirect()->route('todos')->with('success','Tarea creada correctamente');
+    }
 }
--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
     <div class="container w-25 border p-4 mt-4">
-        <form>
+        <form action="{{route('todos')}}" method="POST">
+            @csrf
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Titulo de la tarea</label>
                 <input type="text" name="title" class="form-control">
